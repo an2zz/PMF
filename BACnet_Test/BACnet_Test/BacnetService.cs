9aa9efb5a7e1e2996bdbe4925d94bbc0573d4aa1
an2zz <so_cooll_@naver.com>
2021-02-01 17:32:01 +0900

    BACnet_Yabe_Wireshark

---
+++ BACnet_Test/BACnet_Test/BacnetService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.BACnet;
+
+namespace BACnet_Test
+{
+    class BacnetService : BacnetClient
+    {
+        public BacnetService(IBacnetTransport transport) : base(transport)
+        {
+        }
+
+        public void SendWhois()
+        {
+            try
+            {
+                WhoIs(-1, -1, new BacnetAddress(BacnetAddressTypes.IP, "192.168.0.65"));
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ BACnet_Test/BACnet_Test/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.BACnet;
+
+namespace BACnet_Test
+{
+    public partial class Form1 : Form
+    {
+        private BacnetService bacnet_service;
+        public Form1()
+        {
+            InitializeComponent();
+            bacnet_service = new BacnetService(new BacnetIpUdpProtocolTransport(0xBABB, false, false, 1472, "192.168.0.65"));
+            bacnet_service.Start();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bacnet_service.SendWhois();
+            //Console.WriteLine(bacnet_service);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            bacnet_service.SendWhois();
+        }
+    }
+}
